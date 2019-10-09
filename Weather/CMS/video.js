ad42f620982f668677fce8ec627b7a31ad9e137f
trungdang97@gmail.com <trungdang97@gmail.com>
2019-10-10 01:22:26 +0700

    build 11.2: add upload video

--- Weather/CMS/video.js
+++ Weather/CMS/video.js
@@ -2,6 +2,18 @@
 
 $(document).ready(function () {
     GetVideos();
+
+    $("#BtnSave").click(async function () {
+        await CreateVideo();
+    });
+
+    $("#BtnReset").click(function () {
+        modalReset();
+    });
+
+    $("#File").change(function () {
+        $("#FileName").val(($("#File").get(0).files[0].name));
+    });
 });
 
 var GetVideos = function () {
@@ -27,19 +39,77 @@ var ShowListVideo = function () {
             + "<td>" + listVideos[i].Name + "</td>"
             + "<td class='text-center'>" + FormatDateTime(listVideos[i].CreatedOnDate) + "</td>"
             + "<td>" + listVideos[i].FullPath + "</td>"
-            //+ "<td class='text-center'><button type='button' value='" + listVideos[i].Id + "' class='btn btn-danger' onclick='DeleteVideo(this)'><i class='fa fa-trash'></i></button></td>"
+            + "<td class='text-center'><button type='button' value='" + listVideos[i].Id + "' class='btn btn-danger' onclick='DeleteVideo(this)' data-toggle='modal' data-target='#myModal'><i class='fa fa-trash'></i></button></td>"
             + "</tr>")
     }
 };
 
-var DeleteVideo = function (btn) {
+var modalReset = function () {
+    $("#Name").val("");
+    $("#FileName").val("");
+    $("#File").val("");
+    $('#exampleModal').modal('hide');
+}
+
+var CreateVideo = function () {
+    var video = {};
+    video.Name = $("#Name").val();
+    video.FullPath = $("#FileName").val();
+
+    //video.File = $("#File").get(0).files[0];
+
     $.ajax({
-        url: "/api/v1/videos?id=" + btn.value,
-        method: "DELETE",
+        url: "/api/v1/videos/create",
+        method: "POST",
+        data: JSON.stringify(video),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
         contentType: "json",
-        dataType:"json",
+        success: function (response) {
+            var data = new FormData();
+
+            var files = $("#File").get(0).files;
+
+            // Add the uploaded image content to the form data collection
+            if (files.length > 0) {
+                data.append("UploadedVideo", files[0]);
+            }
+
+            // Make Ajax request with the contentType = false, and procesDate = false
+            var ajaxRequest = $.ajax({
+                type: "POST",
+                url: "/api/v1/videos/upload",
+                contentType: false,
+                processData: false,
+                data: data
+            });
+
+            ajaxRequest.done(function (xhr, textStatus) {
+                alert("Upload video thành công!");
+                modalReset();
+                GetVideos();
+            });
+        },
+        error: function (response) {
+            console.log(response);
+        }
+    });
+};
+
+var videoId = null;
+var DeleteVideo = function (btn) {
+    videoId = btn.value;
+};
+
+var Delete = function () {
+    $.ajax({
+        url: "/api/v1/videos/delete?id=" + videoId,
+        method: "DELETE",
         success: function (response) {
             alert("Xóa video thành công!");
+            GetVideos();
         },
         error: function (response) {
             console.log(response);
--- Weather/Controllers/VideoController.cs
+++ Weather/Controllers/VideoController.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 using Weather.Data;
 
@@ -25,6 +26,12 @@ namespace Weather.Controllers
         }
     }
 
+    public class CMS_VideoCreateRequest
+    {
+        public string Name { get; set; }
+        public string FullPath { get; set; }
+    }
+    
     public class VideoController : ApiController
     {
         [HttpGet]
@@ -49,7 +56,7 @@ namespace Weather.Controllers
         {
             using (var db = new cms_VKTTVEntities())
             {
-                var data = await db.cms_Video.OrderBy(x => x.CreatedOnDate).ToListAsync();
+                var data = await db.cms_Video.OrderByDescending(x => x.CreatedOnDate).ToListAsync();
                 var result = new List<CMS_Video>();
                 foreach (var dt in data)
                 {
@@ -59,17 +66,63 @@ namespace Weather.Controllers
             }
         }
 
-        [HttpDelete]
-        [Route("api/v1/videos/{id}")]
-        public async Task<CMS_Video> DeleteVideo(string id)
+        [HttpPost]
+        [Route("api/v1/videos/create")]
+        public async Task<CMS_Video> CreateVideo([FromBody]CMS_VideoCreateRequest model)
         {
             using (var db = new cms_VKTTVEntities())
             {
-                var data = db.cms_Video.Where(x => x.Id == Guid.Parse(id)).FirstOrDefault();
+                cms_Video video = new cms_Video()
+                {
+                    Id = Guid.NewGuid(),
+                    CreatedOnDate = DateTime.Now,
+                    Name = model.Name,
+                    FullPath = model.FullPath
+                };
+                db.cms_Video.Add(video);
+                if(await db.SaveChangesAsync() >= 1)
+                {
+                    
+                }
+
+                return new CMS_Video(video);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/v1/videos/upload")]
+        public void UploadVideo()
+        {
+            if (HttpContext.Current.Request.Files.AllKeys.Any())
+            {
+                // Get the uploaded image from the Files collection
+                var httpPostedFile = HttpContext.Current.Request.Files["UploadedVideo"];
+
+                if (httpPostedFile != null)
+                {
+                    // Validate the uploaded image(optional)
+
+                    // Get the complete file path
+                    var fileSavePath = Path.Combine(HttpContext.Current.Server.MapPath("~/videos"), httpPostedFile.FileName);
+
+                    // Save the uploaded file to "UploadedFiles" folder
+                    httpPostedFile.SaveAs(fileSavePath);
+                }
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/v1/videos/delete")]
+        public async Task<CMS_Video> DeleteVideo(Guid id)
+        {
+            using (var db = new cms_VKTTVEntities())
+            {
+                var data = db.cms_Video.Where(x => x.Id == id).FirstOrDefault();
                 db.cms_Video.Remove(data);
                 if(await db.SaveChangesAsync() >= 1)
                 {
-                    var path = System.Web.Hosting.HostingEnvironment.MapPath("~");
+                    var fileSavePath = Path.Combine(HttpContext.Current.Server.MapPath("~/videos"), data.FullPath);
+                    File.Delete(fileSavePath);
                 }
                 return new CMS_Video(data);
             }
